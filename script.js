87d36561d95d353d658af1a9650efd38b0646b8a
mahmud035 <mahamudulhasan4148@gmail.com>
2022-06-12 04:49:58 +0600

    lecture 21 done

--- script.js
+++ script.js
@@ -95,17 +95,139 @@ console.log(result); */
 
 //* #13 - Array reduce() method
 
-const numbers = [1, 2, 3, 4, 5, 6];
+/* const numbers = [1, 2, 3, 4, 5, 6];
 
 const result = numbers.reduce((prevValue, currentValue) => {
   return prevValue + currentValue;
 }, 0);
 
-console.log(result);
+console.log(result); */
 
 //* #14 - for loop, for in & for of
 
+//* #17 - Spread Operator
+/* const numbers = [1, 2, 3, 4];
+
+const newNumbers = [...numbers, 5, 6, 7]; // copy of numbers array
+
+console.log(numbers);
+console.log(newNumbers);
+
+const numbers1 = [1, 2, 3, 4];
+const numbers2 = [5, 6, 7, 8];
+
+const totalNumbers = [...numbers1, ...numbers2]; // concat two arrays
+console.log(totalNumbers); */
+
+//* #18 - Rest Operator
+
+/* function myFunc(a, b, ...params) {
+  console.log(a);
+  console.log(b);
+  console.log(params);
+}
+
+myFunc(1, 2, 3, 4, 5, 6); */
+
+//* #19 - Destructuring
+
+// Object destructuring
+// Example: 1
+/* const person = {
+  name: 'John',
+  age: 30,
+  location: {
+    city: 'Miami',
+    temp: 92,
+  },
+};
+
+const {
+  name,
+  age,
+  location: { city, temp },
+} = person;
+
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(temp); */
+
+// Example 2
+/* const user = {
+  id: 334,
+  name: 'John',
+  age: 30,
+  // education: {
+  //   degree: 'Masters',
+  // },
+};
+
+const { education: { degree } = {} } = user; // if education is not defined, default value is {}
+
+console.log(degree); */
+
+// Array destructuring
+// Example: 1
+
+/* const numbers = [1, 2, 3, 4, 5];
+
+const [num1, num2] = numbers;
+const [, , num3, , num5] = numbers;
+
+console.log(num1, num2);
+console.log(num3, num5); */
+
+// Example: 2 (Nested array destructuring)
+
+/* const numbers2 = [5, 6, [7, 8, 9], 10];
+
+const [, , [, a, b]] = numbers2;
+
+console.log(a, b); */
+
+// Example: 3 (Value Swapping)
+
+/* let x = 1;
+let y = 2;
+
+[x, y] = [y, x];
+
+console.log(x, y); */
+
+//* #20 - Module Imports / Exports
+
+/* import { PI, a } from './external.js'; // named import
+
+console.log(PI, a); */
+
+//* #21 - Template Literals
+
+// Example: 1
+
+/* let firstName = 'John';
+let lastName = 'Doe';
+let age = 30;
+
+let text = `Welcome ${firstName} ${lastName}! You are ${age} years old.`;
+console.log(text); */
+
+// Example: 2
+
+/* let price = 10;
+let VAT = 0.25;
+
+let totalPrice = `Total: ${(price * (1 + VAT)).toFixed(2)}`;
+console.log(totalPrice); */
+
+//* #22 - Tagged Template Literals
 
 
 
+/* let player1 = 'Sakib';
+let player2 = 'Tamim';
 
+console.log(`We have ${player1} and ${player2} in our cricket team`); */
+
+
+//* #23 - Set & WeakSet 
